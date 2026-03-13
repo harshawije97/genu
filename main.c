1d450d152ed8955fe6f580c1ec69e19ed96612c7
harshawije97 <harshawije99@gmail.com>
2026-03-13 14:10:15 +0530

    data types exploring

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 // preprocessor directives
-
 #include <stdio.h>
 
 int main()
 {
-    printf("Hello World👋\nMy name is Harshana 🥷");
+    float number = 34.56;
+    int number2 = 10;
+
+    printf("Number 1 init %d,\nNumber 2 init: %d, \n", number, number2);
     return 0;
 }
